fe21debfd31f6b84a31f22bee0c707967bb69242
Rajagopal-2211 <rajagopalreddy2211@gmail.com>
2025-02-12 11:39:45 +0530

    Messsage

--- src/App.js
+++ src/App.js
@@ -1,109 +1,13 @@
-// // import logo from './logo.svg';
-// // import './App.css';
-
-// // function App() {
-// //   return (
-// //     <div className="App">
-// //       <header className="App-header">
-// //         <img src={logo} className="App-logo" alt="logo" />
-// //         <p>
-// //           Edit <code>src/App.js</code> and save to reload.
-// //         </p>
-// //         <a
-// //           className="App-link"
-// //           href="https://reactjs.org"rafc
-// //           target="_blank"
-// //           rel="noopener noreferrer"
-// //         >
-// //           Learn React
-// //         </a>
-// //       </header>
-// //     </div>
-// //   );
-// // }
-
-// // export default App;
-// // import React, { Component } from 'react'
-
-// // export default class App extends Component {
-// //   render() {
-// //     return (
-// //       <div>
-        
-// //       </div>
-// //     )
-// //   }
-// // }
-// import React,{Component} from 'react'
-// import CBCPropEx1 from './propexample/CBCPropEx1';
-
-// export default class App extends Component{
-//   render() {
-//     return (
-//       <div>
-//         App
-//         <CBCPropEx1
-//         username="RajaGopal"
-//         age={21}
-//         hobbies={["playing","cooking","sleeping"]}
-//         address={{city:"Hyd",area:"misammaguda"}}
-//         isMarried={function(){alert("Hii Pramodh")}}
-//         />
-//       </div>
-//     )
-//   }
-// }
-// import React from 'react'
-
-// export default function App() {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-// import React from 'react'
-// import ProsChildrenex from './propexample/PropsChildrenex'
-// import SubChildProps from './propexample/SubChildProps'
-
-// const App =() => {
-//   return (
-//       <div>
-//         <ProsChildrenex username="RajuGopal" company="Meta">
-//           <h1>This data is Passing as a Props children to child company</h1>
-//           <SubChildProps/>
-//           </ProsChildrenex>
-        
-//       </div>
-//     )
-//   }
-// export default App
-
-// import React from 'react'
-// import FunctionalComponents from './components/FunctionalComponents'
-
-// const App = ()  {
-//   return (
-//     <div> className="App  "
-//       <h1>Components App</h1>
-//       <FunctionalComponents/>
-//     </div>
-//   )
-// }
-
-// export default App
 import React from 'react'
-import RefExample from './components/RefExample'
+import Navbar from './components/Navbar'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
     <div>
-      
-      <RefExample/>
-      
+      <Navbar/>
     </div>
   )
 }
 
 export default App
-
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styles from "./navbar.module.css"
+
+const Logo = () => {
+  return (
+    <div className={Style.logoBlock}>
+    <img>https://media.geeksforgeeks.org/gfg-gg-logo.svg</img>
+      
+    </div>
+  )
+}
+
+export default Logo
